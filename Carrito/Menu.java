68cec6c5ae56889160ddad77644052a1bb007d58
joellpz <joel@joel.com>
2022-10-10 16:43:13 +0200

    Menu creation

--- Carrito/Menu.java
+++ Carrito/Menu.java
@@ -1,7 +1,28 @@
 package Carrito;
 
 public class Menu {
-    public void Menu() {
-        System.out.println("hola");
+    public void shoppingMenu() {
+        String[] options = {"Introduir Producte",
+                "Passar per caixa",
+                "Mostrar carro de la compra",
+                "Sortir"};
+        System.out.println("--- Tria una opció ---");
+        for (int i = 0; i < options.length; i++) {
+            if (i == options.length-1) System.out.println("0. "+ options[i]);
+            else System.out.println((i+1) +". "+ options[i]);
+        }
+    }
+
+    public void productMenu(){
+        String[] options = {"Alimentació",
+                "Téxtil",
+                "Electrònica",
+                "Tornar"};
+
+        System.out.println("--- Tria una opció ---");
+        for (int i = 0; i < options.length; i++) {
+            if (i == options.length-1) System.out.println("0. "+ options[i]);
+            else System.out.println((i+1) +". "+ options[i]);
+        }
     }
 }
