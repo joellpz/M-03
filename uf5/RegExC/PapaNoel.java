0459a036ae22f99f4471dec5e5d148178e777b79
joellpz <joelme001@gmail.com>
2022-11-09 21:30:07 +0100

    REGEX BIEN

--- uf5/RegExC/PapaNoel.java
+++ uf5/RegExC/PapaNoel.java
@@ -1,27 +1,30 @@
 package uf5.RegExC;
 
-import java.io.*;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PapaNoel {
-    private static String[] values = {"Papa Noel", "Ren", "Follet"};
-    private static String[] regex = {"\\*<]:-DOo", ">:o\\)", "<]:-D"};
-    private static Map<String, Integer> reps = new HashMap<>();
+    private static final String[] values = {"Papa Noel", "Ren", "Follet"};
+    private static final String[] regex = {"\\*<]:-DOo", ">:o\\)", "<]:-D"};
+    private static final String[] regexString = {"*<]:-DOo", ">:o)", "<]:-D"};
+    private static final Map<String, Integer> reps = new HashMap<>();
     static String line;
     static int cont = 0;
 
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("uf5/RegExC/santako.txt"));
-        br.mark(0);
-        regexFind(br);
-        br.reset();
-        stringFind(br);
-        br.close();
+        BufferedReader br_regex = new BufferedReader(new FileReader("uf5/RegExC/santako.txt"));
+        BufferedReader br_string = new BufferedReader(new FileReader("uf5/RegExC/santako.txt"));
+        regexFind(br_regex);
+        System.out.println("------------");
+        stringFind(br_string);
+        br_regex.close();
+        br_string.close();
     }
 
     public static void regexFind(BufferedReader br) throws IOException {
@@ -47,14 +50,31 @@ public class PapaNoel {
     }
 
     public static void stringFind(BufferedReader br) throws IOException {
-        String lineCP = line;
+        String lineCP ;
         while ((line = br.readLine()) != null) {
             for (int i = 0; i < values.length; i++) {
+                lineCP = line;
                 cont = 0;
-                //while(lineCP.contains(regex[i])){
-                System.out.println(lineCP.indexOf(regex[i]));
-                //}
+                //System.out.println(" --> Regex: " + regexString[i] + " Contiene: "+ lineCP.contains(regexString[i]));
+                //System.out.println(" ////" + lineCP);
+                while(lineCP.contains(regexString[i])){
+                    //System.out.println(lineCP);
+
+                    lineCP = lineCP.substring(lineCP.indexOf(regexString[i])+regexString[i].length());
+                    cont++;
+                    //System.out.println(lineCP);
+                    //System.out.println(regexString[i]);
+                    //System.out.println(cont);
+                }
+                if (values[i].equals("Follet")) cont -= reps.get("Papa Noel");
+                reps.put(values[i], cont);
             }
+            reps.forEach((k, v) -> {
+                if (v != 0) {
+                    System.out.print(k + "(" + v + ")" + " ");
+                }
+            });
+            System.out.println();
         }
     }
 }
