1b74b8615bb91a63ae7b579e1854dbf910215c54
joellpz <joel@joel.com>
2022-11-09 17:57:28 +0100

    REGEX

---
+++ uf5/RegExC/PapaNoel.java
@@ -0,0 +1,56 @@
+package uf5.RegExC;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PapaNoel {
+    private static String[] values = {"Papa Noel", "Ren", "Follet"};
+    private static String[] regex = {"\\*<]:-DOo", ">:o\\)", "<]:-D"};
+    private static Map<String, Integer> reps = new HashMap<>();
+    static String line;
+    static int cont = 0;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("uf5/RegExC/santako.txt"));
+
+        while ((line = br.readLine()) != null) {
+            regexFind();
+            stringFind();
+        }
+        br.close();
+
+    }
+
+    public static void regexFind(){
+        for (int i = 0; i < values.length; i++) {
+            cont = 0;
+            Pattern P = Pattern.compile(regex[i]);
+            Matcher m = P.matcher(line);
+            while (m.find()) {
+                cont++;
+            }
+            if (values[i].equals("Follet")) cont -= reps.get("Papa Noel");
+            reps.put(values[i], cont);
+        }
+        reps.forEach((k, v) -> {
+            if (v != 0) {
+                System.out.print(k + "(" + v + ")" + " ");
+            }
+        });
+        System.out.println();
+    }
+
+    public static void stringFind(){
+        for (int i = 0; i < values.length; i++) {
+            cont = 0;
+            for (int j = 0; j < line.split(regex[i]).length; j++) {
+                System.out.println();
+            }
+            ;
+        }
+    }
+}
