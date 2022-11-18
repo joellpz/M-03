3cb669b81ddfe8286dd75483d19e7fa33c8f1f92
joellpz <joel@joel.com>
2022-11-18 18:36:17 +0100

    Fibonacci

--- ThreadsAndThings/FactorialTask/Fibonacci/FibonacciTask.java
+++ ThreadsAndThings/FactorialTask/Fibonacci/FibonacciTask.java
@@ -1,8 +1,9 @@
 package ThreadsAndThings.FactorialTask.Fibonacci;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class FibonacciTask extends RecursiveTask {
+public class FibonacciTask extends RecursiveTask<Long> {
     private int n;
     private static final int LLINDAR = 5;
 
@@ -10,25 +11,34 @@ public class FibonacciTask extends RecursiveTask {
         this.n = n;
     }
 
-    private int fibonacciR() {
-        FibonacciTask fibonacciTask = new FibonacciTask(20);
-        fibonacciTask.fork();
-        return fibonacciTask.join()
+    private Long fibonacciR(int n) {
+        FibonacciTask ft = new FibonacciTask(n-1);
+        ft.fork();
+        return ft.join() * n;
     }
 
-    private int fibonacciS() {
-        int num1 = 1, num2 = 0;
-        for (int i = 1; i < n; i++) {
-            if (i >= 2) num2 = num1;
-            num1 =  num2 + num1;
+    private static Long fibonacciS(int n) {
+        long prev1=0, prev2=1;
+        for(int i=0; i<n; i++) {
+            long savePrev1 = prev1;
+            prev1 = prev2;
+            prev2 = savePrev1 + prev2;
         }
-        return num1;
+        return prev1;
     }
 
     @Override
-    protected int compute() {
-        if(){
-            return fibonacciR();
-        }else return fibonacciS();
+    protected Long compute() {
+        if(n > LLINDAR) {
+            return fibonacciR(n);
+        }else return fibonacciS(n);
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+        FibonacciTask ft = new FibonacciTask(14);
+        pool.invoke(ft);
+        Long result2 = ft.join();
+        System.out.println(result2);
     }
 }
---
+++ uf5/Exceptions/PersonNotBornException.java
@@ -0,0 +1,7 @@
+package uf5.Exceptions;
+
+public class PersonNotBornException extends Exception {
+    public PersonNotBornException(String msg_error_data) {
+        super(msg_error_data);
+    }
+}
